9a24bf9d7ed17e13721e5d9a07cf7ffa05665bc9
micgogi <rahul.gogyani@gmail.com>
2019-12-25 19:14:29 +0530

    Update env-variable.sh

--- favouriteservice/env-variable.sh
+++ favouriteservice/env-variable.sh
@@ -2,5 +2,5 @@ export MYSQL_DATABASE=cricket_database
 export MYSQL_USER=root
 export MYSQL_PASSWORD=root
 if [[ -z "${MYSQL_CI_URL}" ]]; then
-export MYSQL_CI_URL=jdbc:mysql://localhost:3306/cricket_database
+export MYSQL_CI_URL=jdbc:mysql://127.0.0.1:3306/cricket_database
 fi
